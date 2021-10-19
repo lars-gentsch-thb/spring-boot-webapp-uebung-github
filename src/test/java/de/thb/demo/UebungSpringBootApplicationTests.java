0759dbb9e5082ecc8060d4253bc0f1e007c6a64e
Lars Gentsch <lars.gentsch@cloud.ionos.com>
2021-10-19 21:27:07 +0200

    test 123

---
+++ src/test/java/de/thb/demo/UebungSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
